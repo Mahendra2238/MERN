f68a24bd115bfc55ecc9dc1f91a864e0a0fe18e4
Mahendra Gaddam <mahendragaddam997@gmail.com>
2025-09-15 20:08:56 +0530

    Delete src directory

--- src/Components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css'
-function Footer() {
-    return (
-        <div className='footer-div'>
-           <h1 style={{ fontSize: "1.5rem" }}>Developed by Mahendra</h1>
-        </div>
-    )
-}
-export default Footer;
